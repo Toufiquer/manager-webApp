cd9cea89fc3abbfcfb6cbeec040be0809c999f2a
Toufiquer <toufiquerabc@gmail.com>
2024-05-04 01:29:50 +0600

    done @ design

--- src/app/dashboard/outlet-components/dashboard-onboarding.tsx
+++ src/app/dashboard/outlet-components/dashboard-onboarding.tsx
@@ -5,11 +5,43 @@
 | @copyright: Toufiquer, May, 2024
 |-----------------------------------------
 */
+
+"use client";
+
+import { useState } from "react";
+import Board from "./onboarding-sub-components/board";
+import Workflow from "./onboarding-sub-components/workflow";
+
 const DashboardOnboarding = () => {
+  const [currOutlet, setCurrOutlet] = useState("board");
   return (
     <main>
-      <div className="flex items-center justify-center w-full text-4xl h-screen">
-        Dashboard Onboarding
+      <div className="p-4">
+        <div className="w-full flex flex-col">
+          <h2 className="text-slate-700">Onboarding</h2>
+          <div className="flex w-full items-start justify-start border-b gap-4">
+            <h4
+              onClick={() => setCurrOutlet("board")}
+              className={`font-semibold text-slate-500 border-b-4 cursor-pointer border-transparent hover:text-blue-400 ${
+                currOutlet === "board" &&
+                " text-blue-500 border-blue-500 border-b"
+              }`}
+            >
+              Board
+            </h4>
+            <h4
+              onClick={() => setCurrOutlet("workflow")}
+              className={`font-semibold text-slate-500 border-b-4 cursor-pointer border-transparent hover:text-blue-400 ${
+                currOutlet === "workflow" &&
+                " text-blue-500 border-blue-500 border-b"
+              }`}
+            >
+              Workflow
+            </h4>
+          </div>
+          {currOutlet === "board" && <Board />}
+          {currOutlet === "workflow" && <Workflow />}
+        </div>
       </div>
     </main>
   );
